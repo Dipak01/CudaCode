ff758eb30b7569cddf686fbc8f5db579dc0ea8f8
dhruv3 <dhruv.mnit@gmail.com>
2017-03-18 23:21:52 -0400

    updated atomic function

--- atomicSPMV/atomicSPMV/spmv_atomic.c
+++ atomicSPMV/atomicSPMV/spmv_atomic.c
@@ -2,7 +2,7 @@
 #include <sys/time.h>
 //http://berenger.eu/blog/cusparse-cccuda-sparse-matrix-examples-csr-bcsr-spmv-and-conversions/
 
-__global__ void getMulAtomic_kernel(const int nz, const int * coord_row, const int * coord_col, const float * vec, const float * mat, const float * res){
+__global__ void getMulAtomic_kernel(const int nz, const int * coord_row, const int * coord_col, const float * vec, const float * mat, float * res){
 	int threadId = blockDim.x * blockIdx.x + threadIdx.x;
 	int totalThread = blockDim.x * gridDim.x;
 	int iter = (nz % totalThread) ? (nz / totalThread + 1) : (nz / totalThread);
@@ -20,40 +20,38 @@ __global__ void getMulAtomic_kernel(const int nz, const int * coord_row, const i
 }
 
 void getMulAtomic(MatrixInfo * mat, MatrixInfo * vec, MatrixInfo * res, int blockSize, int blockNum){
-    /*Allocate here...*/
 	int *d_coord_row, *d_coord_col;
 	float *d_vec, *d_mat, *d_res;
 
 	//in mmio rIndex = (int *)malloc(nz * sizeof(int));
 	//so size has been set to nz*int
 	//similar for coord_col
-	cudaMalloc(&d_coord_row, mat->nz * sizeof(int));
-	cudaMalloc(&d_coord_col, mat->nz * sizeof(int));
-	cudaMalloc(&d_mat, mat->nz * sizeof(float));
-	cudaMalloc(&d_vec, vec->nz * sizeof(float));
-	cudaMalloc(&d_res, res->nz * sizeof(float));
-	
-	cudaMemset(d_res, 0, res->nz * sizeof(float));
+	cudaMalloc((void**)&d_coord_row, mat->nz * sizeof(int));
+	cudaMalloc((void**)&d_coord_col, mat->nz * sizeof(int));
+	cudaMalloc((void**)&d_mat, mat->nz * sizeof(float));
+	cudaMalloc((void**)&d_vec, mat->N * sizeof(float));
+	cudaMalloc((void**)&d_res, mat->M * sizeof(float));
 
-	cudaMemcpy(d_coord_row, mat->rIndex, nz * sizeof(int), cudaMemcpyHostToDevice);
-	cudaMemcpy(d_coord_col, mat->cIndex, nz * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemset(d_res, 0, mat->M * sizeof(float));
+
+	cudaMemcpy(d_coord_row, mat->rIndex, mat->nz * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_coord_col, mat->cIndex, mat->nz * sizeof(int), cudaMemcpyHostToDevice);
 	cudaMemcpy(d_mat, mat->val, mat->nz * sizeof(float), cudaMemcpyHostToDevice);
-	cudaMemcpy(d_vec, vec->val, vec->nz * sizeof(float), cudaMemcpyHostToDevice);
-	cudaMemcpy(d_res, res->val, res->nz * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_vec, vec->val, mat->N * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_res, res->val, mat->M * sizeof(float), cudaMemcpyHostToDevice);
 
-	/* Sample timing code */
-    struct timespec start, end;
-    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-    /*Invoke kernels...*/
-	getMulAtomic_kernel <<<blockNum, blockSize >>>(mat->nz, d_coord_row, d_coord_col, d_mat, d_vec, d_res);
+	
+	struct timespec start, end;
+	clock_gettime(CLOCK_MONOTONIC_RAW, &start);
 
-    cudaDeviceSynchronize();
-    clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-    /*please modify the timing to milli-seconds*/
-    printf("Atomic Kernel Time: %lu micro-seconds\n", 1000000 * (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000);
- 
+	getMulAtomic_kernel << <blockNum, blockSize >> >(mat->nz, d_coord_row, d_coord_col, d_mat, d_vec, d_res);
+	cudaDeviceSynchronize();
+
+	clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+	printf("Atomic Kernel Time: %lu micro-seconds\n", 1000000 * (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000);
+
 	//copying result back
-	cudaMemcpy(res->val, d_res, res->nz * sizeof(float), cudaMemcpyDeviceToHost);
+	cudaMemcpy(res->val, d_res, mat->M * sizeof(float), cudaMemcpyDeviceToHost);
 
 	/*Deallocate.*/
 	cudaFree(d_coord_col);
